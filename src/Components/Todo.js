1ac2e1c733c8bb406d2185060fa3a0f8e5fa148d
SnowSurfer <drinkaziu@gmail.com>
2019-05-09 17:49:37 -0400

    toggle delete btn on hover

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -7,21 +7,42 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'; //allows us to have secondary list items on the right side
 
+import '../styles/Todo.css';
 
-function Todo({ id, task, completed, removeTodo, toggleComplete }) {
-  return (
-    <ListItem>
-      <Checkbox checked={completed} onChange={() => toggleComplete(id)}/>
-      <ListItemText style={{textDecoration: completed ? 'line-through' : 'none'}}>
-        {task}
-      </ListItemText>
-      <ListItemSecondaryAction>
-        <IconButton aria-label='Delete' onClick={() => removeTodo(id)}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
-  )
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isShowing: false
+    }
+  }
+
+  showDelete = () => {
+    this.setState({isShowing: true})
+  }
+
+  hideDelete = () => {
+    this.setState({isShowing: false})
+  }
+
+  render() {
+    const { id, task, completed, removeTodo, toggleComplete } = this.props;
+    return (
+      <div className='Todo'>
+        <ListItem>
+          <Checkbox checked={completed} onChange={() => toggleComplete(id)}/>
+          <ListItemText style={{textDecoration: completed ? 'line-through' : 'none'}}>
+            {task}
+          </ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton className='Todo-delete-btn' aria-label='Delete' onClick={() => removeTodo(id)}>
+              <DeleteIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+      </div>
+    )
+  }
 }
 
 export default Todo; 
