65932a52551f97f341cd4562a9f3c9148e06b9b5
SnowSurfer <drinkaziu@gmail.com>
2019-05-09 14:33:26 -0400

    create TodoList Component and display initial todos

--- src/Components/TodoApp.js
+++ src/Components/TodoApp.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import TodoList from './TodoList';
 
 function TodoApp() {
+  const initialTodos = [
+    { id: 1, task: 'Walk Drin', completed: false },
+    { id: 2, task: 'Ask for treats', completed: true },
+    { id: 3, task: 'Take nap', completed: false }
+  ]
+  const [todos, setTodos] = useState(initialTodos)
   return (
     <Paper 
       style={{
@@ -19,6 +26,7 @@ function TodoApp() {
           <Typography color='inherit'>To-Do</Typography>
         </Toolbar>
       </AppBar>
+      <TodoList todos={todos} />
     </Paper>
   )
 }
