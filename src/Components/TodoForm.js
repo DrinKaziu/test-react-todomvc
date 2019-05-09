cadf973d010b0687b4b4fcee58fd1e135abd7598
SnowSurfer <drinkaziu@gmail.com>
2019-05-09 18:09:21 -0400

    add input validation

--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
+import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
 
 import useInputState from '../hooks/useInputState';
 
@@ -9,21 +10,24 @@ function TodoForm({ addTodo }) {
   const [value, handleChange, reset] = useInputState('');
   return (
     <Paper style={{margin: '1rem 0', padding: '0 1rem'}}>
-      <form 
+      <ValidatorForm
         onSubmit={e => {
           e.preventDefault();
           addTodo(value);
           reset();
         }}
       >
-        <TextField 
+        <TextValidator 
+          autoFocus
           value={value} 
           onChange={handleChange} 
           margin='dense' 
           label='Add New Task' 
           fullWidth
+          validators={['required']}
+          errorMessages={['please enter a task']}
         />
-      </form>
+      </ValidatorForm>
     </Paper>
   )
 }
