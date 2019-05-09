dbf7c2e4b3e81a19e2963eaa3cf358f697a220a6
SnowSurfer <drinkaziu@gmail.com>
2019-05-09 14:41:53 -0400

    style TodoList w/ Material-ui

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 
 function TodoList(props) {
   return (
-    <div>
+    <Paper>
+      <List>
       {props.todos.map(todo => (
-        <li>{todo.task}</li>
+        <div>
+          <ListItem>
+            <ListItemText>{todo.task}</ListItemText>
+          </ListItem>
+          <Divider />
+        </div>
       ))}
-    </div>
+      </List>
+    </Paper>
   )
 }
 
