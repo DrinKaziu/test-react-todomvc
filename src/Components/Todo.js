d9703f0783d4593dd28814d6accd1188c7934c65
SnowSurfer <drinkaziu@gmail.com>
2019-05-09 15:24:12 -0400

    move single Todo inot its own Component

---
+++ src/Components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+
+function Todo({task}) {
+  return (
+    <ListItem>
+      <ListItemText>
+        {task}
+      </ListItemText>
+    </ListItem>
+  )
+}
+
+export default Todo; 
--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -2,19 +2,17 @@ import React from 'react';
 
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+
+import Todo from './Todo';
 
 function TodoList(props) {
   return (
     <Paper>
       <List>
       {props.todos.map(todo => (
-        <div>
-          <ListItem>
-            <ListItemText>{todo.task}</ListItemText>
-          </ListItem>
+        <div key={todo.id}>
+          <Todo {...todo}/>
           <Divider />
         </div>
       ))}
